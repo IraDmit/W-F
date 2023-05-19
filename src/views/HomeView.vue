739571bacf5b7982575cb6c3d1c72f843f59968b
IraDmitrieva <iritrieva@gmail.com>
2023-05-19 20:32:45 +0400

    погода

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" v-model='region'>
+    <div class="reg" v-for="(r, idx) in cloud" :key="idx">
+      {{r}}
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      region: null,
+      cloud: null,
+      key: null
+    }
+  },
+    watch: {
+      async region() {
+        const {data} = await this.axios.get(`http://api.weatherapi.com/v1/current.json?key=81200ab647f143a39fb154726231905&q=${this.region}`)
+        this.cloud = data
+      }
+    },
+  async mounted () {
+    this.key = await this.axios.get('http://api.weatherapi.com/v1/search.json?key=81200ab647f143a39fb154726231905&q=Moscow')
+    this.key = this.key.data
+  },
 }
 </script>
